37c3922634bbc166a553debc12314538cec124d3
Horațiu Udrea <dev@horatiu-udrea.ro>
2024-07-22 10:44:26 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Shopping List
+This is a sample project to create a shopping list app for Android.
+
+### Features
+One screen with
+- one text input field with one button near it, to add items
+- one shopping list with the added items, each with a delete button that removes it
+
+The items should be preserved between app starts.
+
+### What's important
+We just want to see how you move around the IDE, how you structure your code, 
+how you choose and use frameworks and libraries, and how you think in general. 
+No need to implement all features. We'll come up with questions along the way.
---
+++ app/src/main/java/com/example/shoppinglist/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.shoppinglist
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.shoppinglist.ui.theme.ShoppingListTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ShoppingListTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    MainScreen(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MainScreen(modifier: Modifier = Modifier) {
+    Text(
+        text = "Test text",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MainScreenPreview() {
+    ShoppingListTheme {
+        MainScreen()
+    }
+}
